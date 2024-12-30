9fe4a21a24145152ee944e48749d71a36e4d7ec8
jchahal28 <86856124+jchahal28@users.noreply.github.com>
2024-12-30 00:54:10 +0000

    Co-authored-by: oliverchennn <oliverchennn@users.noreply.github.com> Co-authored-by: kamalashree1 <kamalashree1@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
-from utils import generate_random_sentence
+from utils import generate_random_sentence, createAudio, saveAudio
+IMAGE_ADDRESS="https://img.freepik.com/free-vector/hand-drawn-speech-therapy-scenes-collection_52683-78405.jpg"
+AUDIO_FILE="output.wav"
+SCORE_BENCHMARK = 0.98
+GUIDE=["The app will generate a random sentence and its audio. You can play the audio, view the sentence, or do both.",
+       "The you can click on the **Start Recording** button and start speaking out the text you see.",
+       "Then click on **Stop Recording** to start the evaluations.",
+        "Based on the evaluation you can start again if you like."]
+
+def markdown_creators(*args):
+    for chunks in args:
+        st.markdown(f"- {chunks}")
+        
+st.title("Speech Therapist")
+st.image(IMAGE_ADDRESS,caption = "Speech Therapist")
+st.subheader("User Guide: How to Use the App 📒")
+markdown_creators(*GUIDE)
 
 st.title("Random Sentence Generator")
 if st.button("Random Sentence Generator"):
@@ -7,3 +23,4 @@ if st.button("Random Sentence Generator"):
     st.text(f"Generated sentence is: {random_sentence}")
 else:
     st.text("Click the button to generate a sentence")
+    
--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
 from openai import OpenAI
+import os 
+import streamlit as st
+import numpy as np
+from gtts import gTTS
+import io
+import soundfile as sf
+import tempfile
+MODEL_VERSION = "gpt-4o-mini"
+TEXT_MODEL = "text-embedding-ada-002"
+
+# set up your OpenAI API key and Assembly AI key
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
 client = OpenAI()
 def generate_random_sentence():
     response = client.chat.completions.create(
@@ -8,3 +21,22 @@ def generate_random_sentence():
     )
     sentence = response.choices[0].message.content
     return sentence
+
+def createAudio(sentence):
+    tts = gTTS(sentence, lang='en')
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
+        with io.BytesIO() as f:
+            # Save the audio to an in-memory file in MP3 format
+            tts.write_to_fp(f)
+            f.seek(0)
+
+            # Read the audio data from the buffer
+            audio_data, sample_rate = sf.read(f)
+
+            # Save the data to the temporary WAV file
+            sf.write(tmp_file.name, audio_data, sample_rate)
+    return tmp_file
+
+def saveAudio(audio_bytes, file_name):
+    with open(file_name, 'wb') as new_wav_file:
+        new_wav_file.write(audio_bytes)
