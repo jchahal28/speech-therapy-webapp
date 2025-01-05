7a487d810eeebf967e8023063389be7b9a42a2ef
jchahal28 <86856124+jchahal28@users.noreply.github.com>
2025-01-05 02:30:38 +0000

    defined get_transcript, cal_cosine, and sentence_comparer
    Co-authored-by: kamalashree1 <kamalashree1@users.noreply.github.com>
    Co-authored-by: oliverchennn <oliverchennn@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from utils import generate_random_sentence, createAudio, saveAudio
+from streamlit_mic_recorder import mic_recorder
+from utils import generate_random_sentence, createAudio, saveAudio, get_transcript, cal_cosine, sentence_comparer
 IMAGE_ADDRESS="https://img.freepik.com/free-vector/hand-drawn-speech-therapy-scenes-collection_52683-78405.jpg"
 AUDIO_FILE="output.wav"
 SCORE_BENCHMARK = 0.98
@@ -17,10 +18,73 @@ st.image(IMAGE_ADDRESS,caption = "Speech Therapist")
 st.subheader("User Guide: How to Use the App 📒")
 markdown_creators(*GUIDE)
 
-st.title("Random Sentence Generator")
-if st.button("Random Sentence Generator"):
-    random_sentence = generate_random_sentence()
-    st.text(f"Generated sentence is: {random_sentence}")
+def disable_audio_recorder_session():
+    st.session_state.recorder = False
+def recreate_audio_recorder_session():
+    st.session_state.recorder = True
+def shuffle_sentence_and_disable_audio():
+    st.session_state.recorder = False
+    st.session_state.random_sentence = ""
+
+if 'random_sentence' not in st.session_state:
+    st.session_state.random_sentence = ""
+if 'recorder' not in st.session_state:
+    st.session_state.recorder = False
+
+st.header("Generated Audio and Sentence")
+if not st.session_state.random_sentence:
+    text_output = generate_random_sentence()
+    audio_output = createAudio(text_output)
+    st.session_state.random_sentence = text_output
+    st.audio(audio_output.name, format = "audio/wav")
+    st.header("Transcript")
+    st.subheader(text_output)
 else:
-    st.text("Click the button to generate a sentence")
+    audio_rerun_output = createAudio(st.session_state.random_sentence)
+    st.audio(audio_rerun_output.name, format = "audio/wav")
+    st.header("Transcript")
+    st.subheader(st.session_state.random_sentence)
+# if st.button("Random Sentence Generator"):
+#     random_sentence = generate_random_sentence()
+#     st.text(f"Generated sentence is: {random_sentence}")
+# else:
+#     st.text("Click the button to generate a sentence")
+st.header("Record ⏺️")
+audio = mic_recorder(start_prompt="Start Recording 🎙️",
+                     stop_prompt="Stop Recording 🛑",
+                     just_once=False,
+                     use_container_width=False,
+                     callback=recreate_audio_recorder_session,
+                     args=(),
+                     kwargs={},
+                     key=None)
+if st.session_state.recorder:
+    if audio:
+        saveAudio(audio['bytes'], AUDIO_FILE)
+        st.audio(audio['bytes'])
+        with st.spinner("Evaulating Your Response..... ✅"):
+            user_audio_transcript=get_transcript(AUDIO_FILE)
+            if not user_audio_transcript:
+                st.error("Ouch..Error has occured! Please contact the developer.", icon = "🛑")
+                st.stop()
+            cosine_score=cal_cosine(user_audio_transcript,st.session_state.random_sentence)
     
+        st.header("Evaluations 🗒")
+        if cosine_score > SCORE_BENCHMARK:
+            st.subheader("Good Work Mate! 😍")
+            st.subheader("Your score is {}".format(cosine_score))
+            st.markdown("### Your Transcript!")
+            st.write("Missing/Incorrect words are highlighted in red color for you!")
+            st.markdown(f"#### {sentence_comparer(user_audio_transcript, st.session_state.random_sentence)}")
+            st.subheader("You have Good Speaking Capabilities! Keep up ✅")
+            # create a button re try
+            st.button("Wanna Try Again 🤔", on_click=shuffle_sentence_and_disable_audio, key='success_btn')
+        else:
+            st.header("Having Little Bit of Trouble! 🤗")
+            st.subheader("Your score is {}".format(cosine_score))
+            st.markdown("### Your Transcript!")
+            st.write("Missing/Incorrect words are highlighted in red color for you!")
+            st.markdown(f"#### {sentence_comparer(user_audio_transcript, st.session_state.random_sentence)}")
+            st.subheader("Let's Try Again 💪")
+            # create a button re try
+            st.button("Please Try Again 😊", on_click=shuffle_sentence_and_disable_audio, key='failure_btn')
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+import assemblyai as aai
+from sklearn.metrics.pairwise import cosine_similarity
 import os 
 import streamlit as st
 import numpy as np
@@ -11,8 +13,14 @@ TEXT_MODEL = "text-embedding-ada-002"
 
 # set up your OpenAI API key and Assembly AI key
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["ASSEMBLYAI_API_KEY"] = st.secrets["ASSEMBLYAI_API_KEY"]
 
 client = OpenAI()
+
+def get_openai_embeddings(text: str) -> list[float]:
+    response = client.embeddings.create(input=text, model=TEXT_MODEL)
+    return response.data[0].embedding
+
 def generate_random_sentence():
     response = client.chat.completions.create(
         model="gpt-4o-mini",
@@ -40,3 +48,31 @@ def createAudio(sentence):
 def saveAudio(audio_bytes, file_name):
     with open(file_name, 'wb') as new_wav_file:
         new_wav_file.write(audio_bytes)
+
+def get_transcript(filepath):
+    aai.settings.api_key = os.environ["ASSEMBLYAI_API_KEY"]
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(filepath)
+    if transcript.status == aai.TranscriptStatus.error:
+        return None
+    else:
+        return transcript.text
+    
+def cal_cosine(sentence1, sentence2):
+    sentence1_embedded = get_openai_embeddings(sentence1)
+    sentence2_embedded = get_openai_embeddings(sentence2)
+    
+    arrayOne = np.array(sentence1_embedded).reshape(1,-1)
+    arrayTwo = np.array(sentence2_embedded).reshape(1,-1)
+    cos_sim = cosine_similarity(arrayOne, arrayTwo)
+    return cos_sim[0][0]
+
+def sentence_comparer(user_transcript, actual_sentence):
+    user_words = user_transcript.split(" ")
+    final_view = ""
+    for word in user_words:
+        if word in actual_sentence:
+            final_view += word + " "
+        else:
+            final_view +=  f":red[{word}] "
+    return final_view
